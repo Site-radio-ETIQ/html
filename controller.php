8b0468c6a90350ef5129c09bb9c1ea0ca24e13bd
shikamaru73 <74672780+shikamaru73@users.noreply.github.com>
2021-01-11 16:25:58 +0100

    Update controller.php

--- controller.php
+++ controller.php
@@ -1,27 +1,29 @@
-  
 <?php
   // Vérifie qu'il provient d'un formulaire
+  session_start();
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     //identifiants mysql
     $host = "localhost";
     $username = "root";
     $password = "";
-    $database = "radiovoixdurenard";
+    $database = "questions";
     
-    $objet = $_POST["objetForm"];
+    $objet = $_POST["objet"]; 
     $contenuForm = $_POST["contenuForm"];
-    // Verifie si la session utilisateur est bien active  
-    if(isset($_SESSION['personne']) AND !empty($_SESSION['personne'])) {
-		if(isset($plxMotor->aUsers[$_SESSION['personne']]['nomindividu'])) {
-    
-        $nomindividu = $plxMotor->aUsers[$_SESSION['personne']]['nomindividu']; //$nomindividu prend la valeurs du nom du compte de la session en cours
-        
+    $nomForm = $_SESSION["nomcompte"];      // nomform qui sera utilisé pour l'inserssion prend la valeurs du nomdu compte de la session en cours
+    $emailForm = $_SESSION["mailcompte"];
 
-    if (!isset($objet)){
+    if(!isset($_SESSION['nomcompte'])){                 // verification : si pas de session en cours alors ce message
+    print "Connectez vous pour envoyer un message";
+    exit; 
+    }  
+    
+    else{                                               // sinon
+    if (!isset($objet)){                                //si l'objet est vide alors il affiche ce message
       die("S'il vous plaît entrez votre objet");
     }
-    if (!isset($contenuForm) ){
+    if (!isset($contenuForm) ){                         //si le contenu est vide alors il affiche ce message
       die("S'il vous plaît entrez votre message");
     }  
 
@@ -34,19 +36,18 @@
     }  
     
     //préparer la requête d'insertion SQL
-    $statement = $mysqli->prepare("INSERT INTO formulaire (objetForm, contenuForm, nomform) VALUES(?, ?, ?)");  
+    $statement = $mysqli->prepare("INSERT INTO questions (objetForm, contenuForm, nomForm, emailForm) VALUES(?, ?, ?, ?)"); 
+ //     $statement = $mysqli->prepare("INSERT INTO questions (nomForm, emailForm) SELECT nomindividu, mailindividu FROM abonne"); 
     //Associer les valeurs et exécuter la requête d'insertion
-    $statement->bind_param('ss', $objet, $contenuForm, $nomindividu); 
+    $statement->bind_param('ssss', $objet, $contenuForm, $nomForm, $emailForm); 
     
     if($statement->execute()){
-      print "Votre message concernant " . $objet . "!, a bien été envoyé pour cette question: ". $contenuForm;
+      print "Votre message concernant " . $objet . "! a bien été envoyé pour cette question: ". $contenuForm;
     }else{
       print $mysqli->error; 
     }
     
-            
     }
-    }
-      else{ print "Vous devez creer un compte ou vous connecter." ;} // si la session n'a pas été trouvé 
+  
   }
 ?>
